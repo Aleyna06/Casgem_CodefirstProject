4233dd0a28235f5f1afc8ee386ccf7b5ba3857d9
aleyna çelik <celikaleyna71@gmail.com>
2023-07-12 10:13:26 +0300

    Add project files.

---
+++ Casgem_CodefirstProject/DAL/Entities/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Casgem_CodefirstProject.DAL.Entities
+{
+    public class Booking
+    {
+        public int BoookingID { get; set; }
+        public string  CustomerName { get; set; }
+        public string  Destinatiion { get; set; }
+        public string  Duration { get; set; }
+        public string  Mail { get; set; }
+        public DateTime  BookingDate { get; set; }
+        public string  BookingStatus{ get; set; }
+    }
+}
